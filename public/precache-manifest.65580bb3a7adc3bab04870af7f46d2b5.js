139030737390598843dd93f97ae77b5616fe2687
odeszaxoxo <38877578+odeszaxoxo@users.noreply.github.com>
2019-04-12 14:22:37 +0400

    First build

---
+++ public/precache-manifest.65580bb3a7adc3bab04870af7f46d2b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c7f302ac87ec84b4837",
+    "url": "/static/css/main.bd2e404e.chunk.css"
+  },
+  {
+    "revision": "3c7f302ac87ec84b4837",
+    "url": "/static/js/main.aa76cc0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e30940c1bc32cbecb68",
+    "url": "/static/js/2.bc03f473.chunk.js"
+  },
+  {
+    "revision": "56f0c49cb4b43a270830d37fbbce70ff",
+    "url": "/static/media/lisp.56f0c49c.png"
+  },
+  {
+    "revision": "3160a98615f644739d2912d8cb7c0b10",
+    "url": "/index.html"
+  }
+];
